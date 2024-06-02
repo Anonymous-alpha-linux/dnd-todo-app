b4cc390a4bee01dc3a6dea3a2046be4485b207c3
Anonymous-alpha-linux <tinhnt1006@gmail.com>
2024-06-02 15:12:29 +0700

    Setting up Eslint and Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,68 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
     ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'simple-import-sort', 'import'],
+    rules: {
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            {
+                extensions: ['.tsx', '.ts'],
+            },
+        ],
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/explicit-function-return-type': [
+            'error',
+            {
+                allowExpressions: true,
+            },
+        ],
+        'max-len': [
+            'warn',
+            {
+                code: 80,
+            },
+        ],
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'import/prefer-default-export': 'off',
+        'react/prop-types': 'off',
+    },
+    overrides: [
+        {
+            files: ['src/redux/sagas/*'],
+            rules: {
+                '@typescript-eslint/explicit-function-return-type': off,
+            },
+        },
+    ],
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
 }
